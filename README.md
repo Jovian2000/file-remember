e5e96a0b77f29fce21c1b99c3e7ec8e2d3b17573
Jovian <99047257@mydavinci.nl>
2022-03-17 11:28:50 +0100

    Update readme + F1.09.03.O2

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # file-remember
+## F1.09.03.O2 - Wie had wie ook alweer?
+Heb mijn naamlootjes aangepast, met deze code maakt hij steeds nieuwe bestanden aan voor welke persoon wie heeft getrokken.
+Hij controleert ook of de naam van de bestand al bestaat en als dat zo is dan maakt hij een nieuwe naam voor
+``` python
+import json
+import os.path
+
+namesData = json.dumps(namesDict)
+dictionaryData = json.loads(namesData)
+
+num = 1
+titleData = "datanaamlijst" + str(num)
+
+while True:
+    if os.path.exists("C:/Users/Gebruiker/OneDrive/Bureaublad/ICT/file-remember/data/" + titleData):
+        num += 1
+        titleData = "datanaamlijst" + str(num)
+    else:
+        folderData = open("C:/Users/Gebruiker/OneDrive/Bureaublad/ICT/file-remember/data/" + titleData,"x")
+        folderData.writelines(namesData)
+        break
+```
---
+++ naamlootjes.py
@@ -0,0 +1,66 @@
+import random
+import json
+import os.path
+
+names = []
+namesDict = {}
+lottery = []
+namesPulled = []
+nameLimit = 2
+a = ""
+
+print("\nLootjes generator. Voer de namen in. \n(stop = hele programma stoppen)")
+print("Namen:")
+
+while True:
+    names.append(input(""))
+    if "stop" in names:
+        names.clear()
+        break
+    if len(names) > nameLimit:
+        if "klaar" in names:
+            break
+        print("U heeft genoeg namen. Als u de lootjes wilt trekken, typ 'klaar'.")
+    else: 
+        print("U moet nog " + str(nameLimit - len(names) + 1) + " namen invoeren") 
+names.remove("klaar")
+
+names = list(dict.fromkeys(names))
+lottery.extend(names)
+
+def pullingNames():
+    global lottery, namesPulled, a
+    a = random.choice(lottery)
+    namesPulled.append(a)
+    lottery.remove(a)
+
+for x in range(len(names)):
+    if names[x] in lottery: 
+        lottery.remove(names[x])
+        pullingNames()
+        lottery.append(names[x])
+    else:    
+        pullingNames()
+
+for i in range(len(names)):    
+    namesDict.update({names[i]: namesPulled[i]})
+print("")
+for m, n in namesDict.items():
+    print(m + ": " + n)
+
+namesData = json.dumps(namesDict)
+dictionaryData = json.loads(namesData)
+
+num = 1
+titleData = "datanaamlijst" + str(num)
+
+while True:
+    if os.path.exists("C:/Users/Gebruiker/OneDrive/Bureaublad/ICT/file-remember/data/" + titleData):
+        num += 1
+        titleData = "datanaamlijst" + str(num)
+    else:
+        folderData = open("C:/Users/Gebruiker/OneDrive/Bureaublad/ICT/file-remember/data/" + titleData,"x")
+        folderData.writelines(namesData)
+        break
+        
+folderData.close()
